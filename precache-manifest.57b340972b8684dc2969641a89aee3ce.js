56cea85220e1ef473cb90c18f9398e406665f95d
Vitaliy Tymofeyev <vitalikplus@gmail.com>
2020-02-02 20:31:20 +0200

    Updates

---
+++ precache-manifest.57b340972b8684dc2969641a89aee3ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51934b7334eaddc5ec8f68a323371aae",
+    "url": "/fraise-generator/index.html"
+  },
+  {
+    "revision": "6d51d003821ef942481b",
+    "url": "/fraise-generator/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3eb3556a7537bdb558c0",
+    "url": "/fraise-generator/static/js/2.845626f0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/fraise-generator/static/js/2.845626f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d51d003821ef942481b",
+    "url": "/fraise-generator/static/js/main.e326e50f.chunk.js"
+  },
+  {
+    "revision": "24841630bd72d020de2e",
+    "url": "/fraise-generator/static/js/runtime-main.2390877e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fraise-generator/static/media/logo.5d5d9eef.svg"
+  }
+]);
